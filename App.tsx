a72997117c2cf819a9a418562e8d1aeab99d2788
Artem Zaiarnyi <artemzaiarnyi@Artems-MacBook-Air.local>
2023-05-05 07:29:56 +0300

    Added drawer navigation

--- App.tsx
+++ App.tsx
@@ -7,9 +7,65 @@
 
 import React from 'react';
 import {HomeScreen} from './src/screens/Home/HomeScreen';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import PercentIcon from './src/screens/Home/ui/icons/PercentIcon';
+import {ProfileScreen} from './src/screens/ProfileScreen';
+
+export type RootStackParamsList = {
+  Home: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamsList>();
+
+const Drawer = createDrawerNavigator();
+const DrawerNavigator = () => {
+  return (
+    <Drawer.Navigator
+      screenOptions={{
+        headerStyle: {backgroundColor: '#351401'},
+        headerTintColor: '#fff',
+        sceneContainerStyle: {
+          backgroundColor: '#3f2f25',
+        },
+        drawerContentStyle: {
+          backgroundColor: '#351401',
+        },
+        drawerInactiveTintColor: 'white',
+        drawerActiveTintColor: '#351401',
+        drawerActiveBackgroundColor: '#e4baa1',
+      }}>
+      <Drawer.Screen name="Profile" component={HomeScreen} />
+      {/* <Drawer.Screen
+        name="Favorites"
+        component={FavoriteScreen}
+        options={{
+          drawerIcon: ({color, size}) => (
+            <Ionicons color={color} size={size} name="star" />
+          ),
+        }}
+      /> */}
+    </Drawer.Navigator>
+  );
+};
 
 function App(): JSX.Element {
-  return <HomeScreen />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        {/* <Stack.Screen name="Home" component={HomeScreen} /> */}
+        <Stack.Screen
+          name="Drawer"
+          component={DrawerNavigator}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 export default App;
